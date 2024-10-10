24c0bae1d2228793abd41803242d62ce326b8758
sbylash <salsabyla3008@gmail.com>
2024-10-10 21:12:56 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  Widget titleSection = Container(
+  padding: const EdgeInsets.all(...),
+  child: Row(
+    children: [
+      Expanded(
+        /* soal 1*/
+        child: Column(
+          crossAxisAlignment: ...,
+          children: [
+            /* soal 2*/
+            Container(
+              padding: const EdgeInsets.only(bottom: ...),
+              child: const Text(
+                'Wisata Gunung di Batu',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Text(
+              'Batu, Malang, Indonesia',
+              style: TextStyle(...),
+            ),
+          ],
+        ),
+      ),
+      /* soal 3*/
+      Icon(
+       ...,
+        color: ...,
+      ),
+      const Text(...),
+    ],
+  ),
+);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter layout: Shasia Sasa Salsabyla dan 2241720029',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
