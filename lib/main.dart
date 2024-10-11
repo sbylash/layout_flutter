b59fb7bc461127058418554a372a3f83a742a490
sbylash <salsabyla3008@gmail.com>
2024-10-11 08:18:35 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,112 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  Widget titleSection = Container(
-  padding: const EdgeInsets.all(...),
-  child: Row(
-    children: [
-      Expanded(
-        /* soal 1*/
-        child: Column(
-          crossAxisAlignment: ...,
-          children: [
-            /* soal 2*/
-            Container(
-              padding: const EdgeInsets.only(bottom: ...),
-              child: const Text(
-                'Wisata Gunung di Batu',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Text(
-              'Batu, Malang, Indonesia',
-              style: TextStyle(...),
-            ),
-          ],
-        ),
-      ),
-      /* soal 3*/
-      Icon(
-       ...,
-        color: ...,
-      ),
-      const Text(...),
-    ],
-  ),
-);
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter layout: Shasia Sasa Salsabyla dan 2241720029',
+      title: 'Shasia Sasa Salsabyla - 2241720029',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter layout demo'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: Row(
+          children: [
+            const Expanded(
+              flex: 1,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Pantai Kuta",
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      "Pantai Kuta adalah sebuah tempat pariwisata yang terletak di kecamatan Kuta sebelah selatan Kota Denpasar, Bali, Indonesia. Daerah ini merupakan sebuah tujuan wisata turis mancanegara dan telah menjadi objek wisata andalan Pulau Bali sejak awal tahun 1970-an. Pantai Kuta sering pula disebut sebagai pantai matahari terbenam sebagai lawan dari pantai Sanur.",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Row(
+                            children: [
+                              Icon(Icons.star, color: Colors.black),
+                              Icon(Icons.star, color: Colors.black),
+                              Icon(Icons.star, color: Colors.black),
+                              Icon(Icons.star, color: Colors.black),
+                              Icon(Icons.star, color: Colors.black),
+                            ],
+                          ),
+                          Text("210 Reviews")
+                        ]),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Column(
+                          children: [
+                            Icon(Icons.call),
+                            Text(
+                              "CALL",
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Icon(Icons.location_searching),
+                            Text(
+                              "ROUTE",
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Icon(Icons.share),
+                            Text(
+                              "SHARE",
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Image.network(
+                'https://blog.tripcetera.com/id/wp-content/uploads/2020/03/leebudihart_76864081_2484833498431751_3194446755026370817_n.jpg',
+                fit: BoxFit.cover,
+                errorBuilder: (context, error, stackTrace) => const Center(
+                  child: Text("Error loading image"),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
-
